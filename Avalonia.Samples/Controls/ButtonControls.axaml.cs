b12d1a9de2781191c6e3d3b156c04e686ee01379
Berk Acunaş <berkacunas@gmail.com>
2025-04-01 21:30:16 +0300

    Basic SplitButton added.

--- Avalonia.Samples/Controls/ButtonControls.axaml.cs
+++ Avalonia.Samples/Controls/ButtonControls.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
@@ -32,4 +33,30 @@ public partial class ButtonControls : Window
     {
         this.repeatButtonMessage.Text += "\rButton clicked!";
     }
+
+    public void splitButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (e.Source == null)
+            return;
+
+        object? header = ((MenuItem)e.Source).Header;
+        string headerText = (header != null) ? header.ToString() : null;
+
+        switch (header) 
+        {
+            case "Subitem 1":
+                break;
+            case "Subitem 2":
+                break;
+            case "Subitem 3":
+                break;
+            case "Item 2":
+                break;
+            case "Item 3":
+                break;
+            default:
+                throw new ArgumentException("MenuItem Header value error !");
+        }
+    }
+
 }
